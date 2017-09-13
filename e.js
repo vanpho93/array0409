d1e7eb1882f1781be610cafc313bfc9ba25a3fe7
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-13 18:04:03 +0700

    array

---
+++ e.js
@@ -0,0 +1,17 @@
+// static
+
+// const arr2 = new Array();
+// const arr = [1, 'alo', console, console.log];
+
+const arrSubject = ['NodeJS', 'ReactJS', 'Javascript', 'Angular'];
+
+// arrSubject.push('Express');
+// arrSubject.pop();
+// arrSubject.shift();
+// arrSubject.unshift('Java');
+
+// arrSubject.splice(1, 2);
+// arrSubject.splice(2, 0, 'Android', 'iOS');
+
+console.log(arrSubject[100]);
+// console.log(null.x);
